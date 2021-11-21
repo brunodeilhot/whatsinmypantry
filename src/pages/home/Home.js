26ce4114f89c58c85c592b9edec1c0ecb2a248ea
Bruno Deilhot <bruno.deyllot@gmail.com>
2021-11-21 17:18:14 +0000

    home page added

---
+++ src/pages/home/Home.js
@@ -0,0 +1,69 @@
+import {
+    Avatar,
+    Button,
+    List,
+    ListItem,
+    ListItemAvatar,
+    ListItemButton,
+    ListItemText,
+    TextField,
+    Typography,
+  } from "@mui/material";
+  import { useEffect, useState } from "react";
+  import { Link } from "react-router-dom";
+  import api from "../../api/ingredientApi";
+  
+  const Home = () => {
+  
+    const [value, setValue] = useState('');
+    const [ingredients, setIngredient] = useState();
+  
+    function handleChange(e) {
+      setValue(e.target.value);
+    }
+  
+    useEffect(() => {
+      
+      if (value.length >= 3) {
+          api.searchIngredients(value, searchResult => setIngredient(searchResult))
+      }
+  
+    }, [value])
+  
+  
+    const imageBaseURL = 'https://spoonacular.com/cdn/ingredients_100x100/'
+  
+    return (
+      <>
+        <Typography variant="h1">Home Page</Typography>
+        <TextField
+          onChange={handleChange}
+          fullWidth
+          id="ingredient-search"
+          label="Search Ingredients"
+          variant="outlined"
+        ></TextField>
+          <List dense>
+            {ingredients !== undefined
+              ? ingredients.map((ingredient) => (
+              <ListItem key={ingredient.id} disablePadding>
+                  <ListItemButton>
+                          <ListItemAvatar>
+                              <Avatar src={imageBaseURL+ingredient.image} />
+                          </ListItemAvatar>
+                          <ListItemText primary={ingredient.name} />
+                  </ListItemButton>
+              </ListItem>
+              ))
+              : null
+          }
+          </List>
+        <Link to="recipelist">
+          <Button variant="contained">Recipe List</Button>
+        </Link>
+      </>
+    );
+  };
+  
+  export default Home;
+  
