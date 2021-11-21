f4a587c8b9a708c693b38528ecf75c5710fed7a9
Bruno Deilhot <bruno.deyllot@gmail.com>
2021-11-21 17:30:51 +0000

    added initial theme settings

--- src/App.js
+++ src/App.js
@@ -1,8 +1,21 @@
+import { createTheme, responsiveFontSizes, ThemeProvider } from '@mui/material';
 import MainRoutes from './Routes';
+
+const theme = createTheme({
+  typography: {
+      button: {
+          textTransform: 'none'
+      }
+  }
+});
+
+const responsiveTheme = responsiveFontSizes(theme);
 
 function App() {
   return (
+    <ThemeProvider theme={responsiveTheme}>
     <MainRoutes />
+    </ThemeProvider>
   );
 }
 
