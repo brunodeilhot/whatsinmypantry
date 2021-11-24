f0d1bb51a800b5365f331ef02437f881d369c894
Bruno Deilhot <bruno.deyllot@gmail.com>
2021-11-24 22:06:12 +0000

    added home page features

--- src/pages/home/components/MyPantry.js
+++ src/pages/home/components/MyPantry.js
@@ -1,9 +1,19 @@
+import { KitchenRounded } from "@mui/icons-material";
+import { Typography } from "@mui/material";
+import { Box } from "@mui/system";
 
 
 const MyPantry = () => {
 
     return (
-        <p>This is My Pantry</p>
+        <Box>
+            <Box sx={{ display: 'flex', flexDirection: 'column', alignItems: 'center', mt: 6 }}>
+            <KitchenRounded sx={{ fontSize: '20vh', color: 'text.secondary' }} />
+            <Typography variant="h4" component="p" color="text.secondary" >
+                Your pantry is empty.
+            </Typography>
+            </Box>
+        </Box>
     )
 };
 
