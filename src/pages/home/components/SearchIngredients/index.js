0be1c20e3d76a74a762d83c9f7493a5a59647e15
Bruno Deilhot <bruno.deyllot@gmail.com>
2021-12-05 11:23:40 +0000

    commented testing ingredient fetch

--- src/pages/home/components/SearchIngredients/index.js
+++ src/pages/home/components/SearchIngredients/index.js
@@ -3,7 +3,7 @@ import SearchResults from "./SearchResults";
 import { useEffect, useState } from "react";
 import debounce from "@mui/utils/debounce";
 import { useDispatch, useSelector } from "react-redux";
-// import api from "../../../../services/api";
+import { searchIngredients } from "../../../../services/api";
 
 const SearchIngredients = () => {
   const dispatch = useDispatch();
@@ -29,25 +29,25 @@ const SearchIngredients = () => {
   }
 
   useEffect(() => {
-    // if (value.length >= 3) {
-    //   api.searchIngredients(value, (searchResult) =>
-    //     setIngredient(searchResult)
-    //   );
-    // }
-
-    const testIngredients = [
-      { id: 1, name: "tomato", image: "" },
-      { id: 2, name: "banana", image: "" },
-      { id: 3, name: "lettuce", image: "" },
-      { id: 4, name: "broccoli", image: "" },
-      { id: 5, name: "apple", image: "" },
-      { id: 6, name: "apple sauce", image: "" },
-      { id: 7, name: "tomato sauce", image: "" },
-      { id: 8, name: "kiwi", image: "" },
-    ];
     if (value.length >= 3) {
-      return setIngredient(testIngredients);
+        searchIngredients(value, (searchResult) =>
+        setIngredient(searchResult)
+      );
     }
+
+    // const testIngredients = [
+    //   { id: 1, name: "tomato", image: "" },
+    //   { id: 2, name: "banana", image: "" },
+    //   { id: 3, name: "lettuce", image: "" },
+    //   { id: 4, name: "broccoli", image: "" },
+    //   { id: 5, name: "apple", image: "" },
+    //   { id: 6, name: "apple sauce", image: "" },
+    //   { id: 7, name: "tomato sauce", image: "" },
+    //   { id: 8, name: "kiwi", image: "" },
+    // ];
+    // if (value.length >= 3) {
+    //   return setIngredient(testIngredients);
+    // }
     setIngredient(undefined);
   }, [value]);
 
